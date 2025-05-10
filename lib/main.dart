40f89373fada5fbc5a457669e6cc88eb25634787
Taha <sabrytaha854@gmail.com>
2025-05-10 10:09:07 +0300

    change theme and create NotesList

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:note_app/views/notes_list.dart';
 
-void main() {
-  runApp(const NoteApp());
-}
+// void main() {
+//   runApp(const NoteApp());
+// }
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => NoteApp(), // Wrap your app
+  ),
+);
+
 class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(theme: ThemeData(brightness: Brightness.dark),
+    home: NotesList(),
+    );
   }
 }
---
+++ lib/views/notes_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotesList extends StatelessWidget {
+  const NotesList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
